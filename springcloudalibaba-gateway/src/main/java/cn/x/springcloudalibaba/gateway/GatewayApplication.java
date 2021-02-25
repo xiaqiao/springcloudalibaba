a3f111babb5cc9ebb218402f35ca7a6c5e0def74
xiaqiao <156751121@qq.com>
2021-02-25 18:02:00 +0800

    init

---
+++ springcloudalibaba-gateway/src/main/java/cn/x/springcloudalibaba/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package cn.x.springcloudalibaba.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xqa
+ * @since 2021/2/24
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        log.info("项目启动完成");
+    }
+
+}
