18f4f220d55da4cd66abfe604082474ba774b457
xiaqiao <156751121@qq.com>
2021-02-26 17:43:39 +0800

    init

---
+++ springcloudalibaba-gateway/src/main/java/cn/x/springcloudalibaba/gateway/filter/IpFilter.java
@@ -0,0 +1,51 @@
+package cn.x.springcloudalibaba.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.InetSocketAddress;
+
+
+/**
+ * @author xqa
+ * @since 2021/2/26
+ */
+@Component
+@Order(1)
+@Slf4j
+public class IpFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("请求的ip是{}", getIp(request));
+        //todo 拦截黑名单
+        return chain.filter(exchange);
+    }
+
+    private static String getIp(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
+        String ip = headers.getFirst("X-Forwarded-For");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            //多次反向代理后会有多个ip值，第一个ip才是真实ip
+            return ip.split(",")[0];
+        }
+        ip = headers.getFirst("X-Real-IP");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+        InetSocketAddress remoteAddress = request.getRemoteAddress();
+        //开启断言 -ea
+        assert remoteAddress != null;
+        return remoteAddress.getHostString();
+    }
+
+}
---
+++ springcloudalibaba-gateway/src/main/java/cn/x/springcloudalibaba/gateway/filter/LogFilter.java
@@ -0,0 +1,28 @@
+package cn.x.springcloudalibaba.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author xqa
+ * @since 2021/2/26
+ */
+@Component
+@Order(0)
+@Slf4j
+public class LogFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("URL:{}", request.getURI().toString());
+
+        return chain.filter(exchange);
+    }
+}
